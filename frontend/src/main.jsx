dc5291a062ba70545f798b2ab5549942baa7d524
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-09-08 11:30:11 +0530

    Added vercel speed insights in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { UserProvider } from './context/userContext'
 import { ChatProvider } from './context/ChatContext'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserProvider>
       <ChatProvider>
         <App />
+        <SpeedInsights />
       </ChatProvider>
     </UserProvider>
   </StrictMode>,
